6f9adfd9e56f59504991e1d77d2129fb3a653112
Kiet Truong <bearcoding288@gmail.com>
2024-07-04 08:32:49 +0700

    clean:clean-projectclean:project

--- UseCase/obj/Debug/net8.0/UseCase.AssemblyInfo.cs
+++ UseCase/obj/Debug/net8.0/UseCase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UseCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eccd29a114a53fccaf77cdff94c99532c0315fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf2a28e35cb480d3d28a7f6b56f4ee52f93e0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UseCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UseCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
